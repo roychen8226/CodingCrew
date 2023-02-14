483fbb05081ee7fe41a2d1469954cf08d46c5dfb
roychen8226 <96556115+roychen8226@users.noreply.github.com>
2023-02-14 10:12:56 -0500

    Create CodingCrewDec25.java

---
+++ CodingCrewDec25.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class CodingCrewDec25 
+{
+
+    public static void main(String[] args) 
+    {
+        Scanner scan = new Scanner(System.in);
+        char[] handsA;
+        System.out.println("Enter the moves: ");
+        String originalInput = scan.nextLine();
+        handsA = originalInput.toCharArray();
+        for (int i = 0; i < originalInput.length(); i+=2)
+        {
+            if ()
+        }
+    }
+
+    public static int oh()
+    {
+
+    }
+
+}
